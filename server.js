81454c103b24abb415055ea36fa11ef7c429b9d8
Vagish Kapila <vaakapila@gmail.com>
2026-04-18 05:51:17 +0530

    chore: add X-Snapclaps-Version header to detect Railway deploy

--- server.js
+++ server.js
@@ -135,6 +135,7 @@ app.use((req, res, next) => {
   }
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.setHeader('X-Snapclaps-Version', '2.1.0-phase1');
   if (req.method === 'OPTIONS') return res.sendStatus(200);
   next();
 });
